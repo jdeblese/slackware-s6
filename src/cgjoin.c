f704de1935eb3d57f031d6ef95fcda016306dfd5
Jan-willem De Bleser <jw@thescrapyard.org>
2017-07-09 22:41:14 -0700

    cgjoin: fixes for building, change in API

--- src/cgjoin.c
+++ src/cgjoin.c
@@ -1,8 +1,11 @@
 /* ISC license. */
 
-#include "fmtscan.h"
-#include "strerr2.h"
-#include "djbunix.h"
+#include <string.h>
+#include <unistd.h>
+#include <skalibs/types.h>
+#include <skalibs/strerr2.h>
+#include <skalibs/env.h>
+#include <skalibs/djbunix.h>
 
 #define USAGE "cgmove cgroup prog..."
 #define PREFIX "/sys/fs/cgroup/"
@@ -16,7 +19,7 @@ int main (int argc, char const *const *argv, char const *const *envp)
   int fd;
   PROG = "cgmove" ;
   if (argc < 4) strerr_dieusage(100, USAGE) ;
-  len = str_len(argv[1]) ;
+  len = strlen(argv[1]) ;
   {
     char path[len + PREFIXLEN + SUFFIXLEN + 1] ;
     char pid[UINT_FMT + 1] ;
@@ -25,9 +28,9 @@ int main (int argc, char const *const *argv, char const *const *envp)
     k = uint_fmt(pid, getpid()) ;
     pid[k++] = 0 ;
     // Build up the path to the cgrup filesystem
-    byte_copy(path, PREFIXLEN, PREFIX) ;
-    byte_copy(path + PREFIXLEN, len, argv[1]) ;
-    byte_copy(path + PREFIXLEN + len, SUFFIXLEN, SUFFIX) ;
+    memcpy(path, PREFIX, PREFIXLEN);
+    memcpy(path + PREFIXLEN, argv[1], len);
+    memcpy(path + PREFIXLEN + len, SUFFIX, SUFFIXLEN);
     path[PREFIXLEN + len + SUFFIXLEN] = 0 ;
     // Write the pid to the cgroup's tasks file
     if ((fd = open_write(path)) < 0) strerr_dief2x(100, "cannot open cgroup tasks file: ", path) ;
