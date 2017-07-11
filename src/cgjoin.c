f0f57adc0faa31dbf524f94146bebd185a5975f8
Jan-willem De Bleser <jw@thescrapyard.org>
2017-07-10 17:38:25 -0700

    cgjoin: create target cgroup subgroup

--- src/cgjoin.c
+++ src/cgjoin.c
@@ -2,6 +2,8 @@
 
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include <skalibs/types.h>
 #include <skalibs/strerr2.h>
 #include <skalibs/env.h>
@@ -13,6 +15,37 @@
 #define PREFIXLEN 15
 #define SUFFIX "/cgroup.procs"
 #define SUFFIXLEN 13
+
+static int doit (char const *s)
+{
+  if (mkdir(s, 0755) == -1)
+  {
+    if (errno != EEXIST)
+    {
+      strerr_warnwu2sys("mkdir ", s) ;
+      return 111 ;
+    }
+  }
+  return 0 ;
+}
+
+static int doparents (char const *s)
+{
+  size_t n = strlen(s), i = 0 ;
+  char tmp[n+1] ;
+  for (; i < n ; i++)
+  {
+    if ((s[i] == '/') && i)
+    {
+      int e ;
+      tmp[i] = 0 ;
+      e = doit(tmp);
+      if (e) return e ;
+    }
+    tmp[i] = s[i] ;
+  }
+  return doit(s);
+}
 
 int main (int argc, char const *const *argv, char const *const *envp)
 {
@@ -33,6 +66,7 @@ int main (int argc, char const *const *argv, char const *const *envp)
     memcpy(path + PREFIXLEN, argv[1], len);
     memcpy(path + PREFIXLEN + len, SUFFIX, SUFFIXLEN);
     path[PREFIXLEN + len + SUFFIXLEN] = 0 ;
+    doparents(path);
     // Write the pid to the cgroup's tasks file
     if ((fd = open_write(path)) < 0) strerr_dief2x(100, "cannot open cgroup tasks file: ", path) ;
     fd_write(fd, pid, k) ;
